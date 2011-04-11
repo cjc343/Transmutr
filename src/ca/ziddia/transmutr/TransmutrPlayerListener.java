f18e4e7962b0fd784bfd249ea651d76ca8e27cac
cjc343 <cjc343@gmail.com>
2011-04-11 04:36:42 -0700

    NPE fix when block out of range

--- src/ca/ziddia/transmutr/TransmutrPlayerListener.java
+++ src/ca/ziddia/transmutr/TransmutrPlayerListener.java
@@ -26,37 +26,41 @@ public class TransmutrPlayerListener extends PlayerListener {
 
     public void onPlayerInteract(PlayerInteractEvent event) {
         CopyOnWriteArrayList<String> Transmutr = plugin.GetBlocks();
-        int blockId = event.getClickedBlock().getTypeId();
-        Player p = event.getPlayer();
-        if(!plugin.getPermissions().has(p, "Transmutr")){
-            p.sendMessage(ChatColor.RED + "You don't have permssions to transmute blocks!");
-            return;
-        }
-        for (String blockid : Transmutr) {
-            String[] parts = blockid.split(";");
-            if (convertSimple(blockId).equalsIgnoreCase(parts[0])) {
-                Integer index = 1;
-                while (index < parts.length) {
-                    String[] params = parts[index].split(":");
-                    if (Integer.valueOf(params[0]) == 0) {
-                        //event.setCancelled(true);
-                        //blockIdd.setTypeId(0);
-                    } else {
+		try {
+			int blockId = event.getClickedBlock().getTypeId();
+			Player p = event.getPlayer();
+			if(!plugin.getPermissions().has(p, "Transmutr")){
+				p.sendMessage(ChatColor.RED + "You don't have permssions to transmute blocks!");
+				return;
+			}
+			for (String blockid : Transmutr) {
+				String[] parts = blockid.split(";");
+				if (convertSimple(blockId).equalsIgnoreCase(parts[0])) {
+					Integer index = 1;
+					while (index < parts.length) {
+						String[] params = parts[index].split(":");
+						if (Integer.valueOf(params[0]) == 0) {
+							//event.setCancelled(true);
+							//blockIdd.setTypeId(0);
+						} else {
 
-                        if (Math.random() < Double.valueOf(params[2])) {
-                            if (event.getPlayer().getInventory().getItemInHand().getTypeId() == Integer.parseInt(params[0])) {
-                                event.getClickedBlock().setTypeId(Integer.valueOf(params[1]));
-                                ItemStack old = new ItemStack(event.getPlayer().getItemInHand().getTypeId(), event.getPlayer().getItemInHand().getAmount() - 1);
-                                event.getPlayer().setItemInHand(old);
-                            }
-                        } else {
-                            ItemStack old = new ItemStack(event.getPlayer().getItemInHand().getTypeId(), event.getPlayer().getItemInHand().getAmount() - 1);
-                            event.getPlayer().setItemInHand(old);
-                        }
-                    }
-                    index++;
-                }
-            }
-        }
+							if (Math.random() < Double.valueOf(params[2])) {
+								if (event.getPlayer().getInventory().getItemInHand().getTypeId() == Integer.parseInt(params[0])) {
+									event.getClickedBlock().setTypeId(Integer.valueOf(params[1]));
+									ItemStack old = new ItemStack(event.getPlayer().getItemInHand().getTypeId(), event.getPlayer().getItemInHand().getAmount() - 1);
+									event.getPlayer().setItemInHand(old);
+								}
+							} else {
+								ItemStack old = new ItemStack(event.getPlayer().getItemInHand().getTypeId(), event.getPlayer().getItemInHand().getAmount() - 1);
+								event.getPlayer().setItemInHand(old);
+							}
+						}
+						index++;
+					}
+				}
+			}
+		} catch (NullPointerException npe) {
+			// pointing at air or block out of reach
+		}
     }
 }
